fe8256549cd288c38b3f99764bab4145060cd9fd
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2023-12-06 14:32:10 +0600

    home-3

---
+++ app/home-3/page.js
@@ -0,0 +1,29 @@
+import CoFeature from '@/components/3/CoFeature'
+import Contactus from '@/components/3/Contactus'
+import Footer3 from '@/components/3/Footer3'
+import Growth from '@/components/3/Growth'
+import Header from '@/components/3/Header'
+import Portfolio3 from '@/components/3/Portfolio3'
+import Slide3 from '@/components/3/Slide3'
+import Test from '@/components/3/Test'
+import Testimo from '@/components/3/Testimo'
+import Trusted from '@/components/3/Trusted'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+       
+        <Slide3/>
+        <Trusted/>
+        <CoFeature/>
+        <Growth/>
+        <Portfolio3/>
+        <Testimo/>
+        <Contactus/>
+        <Footer3/>
+    </div>
+  )
+}
+
+export default page
